108eb73cbe1ffcc7b4f983fc600022cd0e4ffb4c
Matthew Casperson <matthewcasperson@gmail.com>
2019-05-23 16:08:04 +1000

    setting initial user

--- octopus.pp
+++ octopus.pp
@@ -1,16 +1,5 @@
 include chocolatey
 
-# We want to run Jenkins in development mode, skipping the initial wizard
-# https://wiki.jenkins.io/display/JENKINS/Features+controlled+by+system+properties
-windows_env { 'JENKINS_JAVA_OPTIONS':
-  ensure    => '-Djenkins.install.runSetupWizard=false',
-  mergemode => clobber,
-}
-
-file { 'C:/packages':
-    ensure => 'directory',
-}
-
 package { '7zip':
   ensure   => installed,
   provider => chocolatey
@@ -41,3 +30,40 @@ package { 'octopusdeploy.tentacle':
   provider => chocolatey
 }
 
+
+file { 'C:/program Files (x86)/Jenkins/init.groovy.d':
+    ensure => 'directory',
+}
+
+file { 'C:/program Files (x86)/Jenkins/init.groovy.d/security.groovy':
+  ensure  => 'file',
+  owner   => 'Administrators',
+  group   => 'Administrators',
+  mode    => '0644',
+  content => @(EOT)
+    #!groovy
+    import java.util.logging.Level
+    import java.util.logging.Logger
+    import hudson.security.*
+    import jenkins.model.*
+
+    def instance = Jenkins.getInstance()
+    def logger = Logger.getLogger(Jenkins.class.getName())
+
+    logger.log(Level.INFO, "Ensuring that local user 'admin' is created.")
+
+    if (!instance.isUseSecurity()) {
+        logger.log(Level.INFO, "Creating local admin user 'admin'.")
+
+        def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+        strategy.setAllowAnonymousRead(false)
+
+        def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+        hudsonRealm.createAccount("admin", "Password01!")
+
+        instance.setSecurityRealm(hudsonRealm)
+        instance.setAuthorizationStrategy(strategy)
+        instance.save()
+    }
+    | EOT
+}
