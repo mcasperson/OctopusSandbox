16a923d40df28990647dbcaf48e88ba8870a04a1
mcasperson <matthewcasperson@gmail.com>
2019-05-23 15:17:29 +1000

    no message

--- install.bat
+++ install.bat
@@ -5,6 +5,6 @@
 
 msiexec /qn /norestart /i https://downloads.puppetlabs.com/windows/puppet5/puppet-agent-x64-latest.msi PUPPET_MASTER_SERVER=puppet
 call "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs/windows
-call "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" --disable_warnings=deprecations apply C:\Users\WDAGUtilityAccount\Desktop\OctopusSandbox\octopus.pp
+call "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" apply C:\Users\WDAGUtilityAccount\Desktop\OctopusSandbox\octopus.pp --disable_warnings=deprecations
 
 @echo The script has completed!
