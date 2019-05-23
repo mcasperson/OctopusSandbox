8d0f2dc73eebac77db13557be7669cdb859a4a49
Matthew Casperson <matthewcasperson@gmail.com>
2019-05-23 15:30:11 +1000

    Running jenkins in development mode

--- octopus.pp
+++ octopus.pp
@@ -1,5 +1,12 @@
 include chocolatey
 
+# We want to run Jenkins in development mode, skipping the initial wizard
+# https://wiki.jenkins.io/display/JENKINS/Features+controlled+by+system+properties
+windows_env { 'JENKINS_JAVA_OPTIONS':
+  ensure    => '-Djenkins.install.runSetupWizard=false',
+  mergemode => clobber,
+}
+
 file { 'C:/packages':
     ensure => 'directory',
 }
