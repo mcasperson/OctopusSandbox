5ff7f24d5b7e5cc9597633f0366c4a8934561d8e
Matthew Casperson <matthewcasperson@gmail.com>
2019-05-23 19:23:23 +1000

    Configured simple theme plugin

--- octopus.pp
+++ octopus.pp
@@ -84,7 +84,7 @@ file { 'C:/program Files (x86)/Jenkins/init.groovy.d':
   subscribe => Package['jenkins'],
 }
 
-file { 'C:/Program Files (x86)/Jenkins/init.groovy.d/security.groovy':
+file { 'C:/Program Files (x86)/Jenkins/init.groovy.d/1.security.groovy':
   ensure    => 'file',
   subscribe => File['C:/program Files (x86)/Jenkins/init.groovy.d'],
   owner     => 'Administrators',
@@ -115,9 +115,9 @@ file { 'C:/Program Files (x86)/Jenkins/init.groovy.d/security.groovy':
     | EOT
 }
 
-file { 'C:/Program Files (x86)/Jenkins/init.groovy.d/plugins.groovy':
+file { 'C:/Program Files (x86)/Jenkins/init.groovy.d/2.plugins.groovy':
   ensure    => 'file',
-  subscribe => File['C:/Program Files (x86)/Jenkins/init.groovy.d/security.groovy'],
+  subscribe => File['C:/Program Files (x86)/Jenkins/init.groovy.d/1.security.groovy'],
   owner     => 'Administrators',
   group     => 'Administrators',
   mode      => '0644',
@@ -180,8 +180,32 @@ file { 'C:/Program Files (x86)/Jenkins/init.groovy.d/plugins.groovy':
     | EOT
 }
 
+file { 'C:/Program Files (x86)/Jenkins/init.groovy.d/3.simpletheme.groovy':
+  ensure    => 'file',
+  subscribe => File['C:/Program Files (x86)/Jenkins/init.groovy.d/2.plugins.groovy'],
+  owner     => 'Administrators',
+  group     => 'Administrators',
+  mode      => '0644',
+  content   => @(EOT)
+    #!groovy
+    import jenkins.model.Jenkins;
+    import org.jenkinsci.plugins.simpletheme.CssUrlThemeElement;
+
+    Jenkins jenkins = Jenkins.get()
+
+    def themeDecorator = jenkins.getExtensionList(org.codefirst.SimpleThemeDecorator.class).first()
+
+    themeDecorator.setElements([
+      new CssUrlThemeElement('https://cdn.rawgit.com/afonsof/jenkins-material-theme/gh-pages/dist/material-blue.css')
+    ])
+
+    jenkins.save()
+
+    | EOT
+}
+
 file_line { 'installStateName':
-  subscribe => File['C:/Program Files (x86)/Jenkins/init.groovy.d/plugins.groovy'],
+  subscribe => File['C:/Program Files (x86)/Jenkins/init.groovy.d/3.simpletheme.groovy'],
   path      => 'C:/Program Files (x86)/Jenkins/config.xml',
   line      => '  <installStateName>RUNNING</installStateName>',
   match     => '^\s*<installStateName>NEW</installStateName>',
