ce3dbf804ade40aa6984332aed3840ffe628e62a
Matthew Casperson <matthewcasperson@gmail.com>
2019-05-23 17:32:25 +1000

    Adding a plugin install script

--- octopus.pp
+++ octopus.pp
@@ -38,6 +38,69 @@ file { 'C:/Program Files (x86)/Jenkins/init.groovy.d/security.groovy':
     | EOT
 }
 
+file { 'C:/Program Files (x86)/Jenkins/init.groovy.d/plugins.groovy':
+  ensure  => 'file',
+  owner   => 'Administrators',
+  group   => 'Administrators',
+  mode    => '0644',
+  content => @(EOT)
+    #!groovy
+    import hudson.model.UpdateSite
+    import hudson.PluginWrapper
+
+    /*
+      Install Jenkins plugins
+    */
+    def install(Collection c, Boolean dynamicLoad, UpdateSite updateSite) {
+        c.each {
+            println "Installing ${it} plugin."
+            UpdateSite.Plugin plugin = updateSite.getPlugin(it)
+            Throwable error = plugin.deploy(dynamicLoad).get().getError()
+            if(error != null) {
+                println "ERROR installing ${it}, ${error}"
+            }
+        }
+        null
+    }
+
+    // Some useful vars to set
+    Boolean hasConfigBeenUpdated = false
+
+    // The default update site
+    UpdateSite updateSite = Jenkins.getInstance().getUpdateCenter().getById('default')
+
+    // The list of plugins to install
+    Set<String> plugins_to_install = [
+        "git", "github", "blueocean"
+    ]
+
+    List<PluginWrapper> plugins = Jenkins.instance.pluginManager.getPlugins()
+
+    //get a list of installed plugins
+    Set<String> installed_plugins = []
+    plugins.each {
+      installed_plugins << it.getShortName()
+    }
+
+    //check to see if there are missing plugins to install
+    Set<String> missing_plugins = plugins_to_install - installed_plugins
+    if(missing_plugins.size() > 0) {
+        println "Install missing plugins..."
+        install(missing_plugins, false, updateSite)
+        println "Done installing missing plugins."
+        hasConfigBeenUpdated = true
+    }
+
+    if(hasConfigBeenUpdated) {
+        println "Saving Jenkins configuration to disk."
+        Jenkins.instance.save()
+    } else {
+        println "Jenkins up-to-date.  Nothing to do."
+    }    
+
+    | EOT
+}
+
 package { '7zip':
   ensure   => installed,
   provider => chocolatey
