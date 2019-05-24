dbae13a1a1912ae3610a395dcff830f2a94c21a4
Matthew Casperson <matthewcasperson@gmail.com>
2019-05-24 10:27:01 +1000

    Fixing installer

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/mcasperson/OctopusSandbox/master/octopus.pp -OutFile octopus.pp
-msiexec /qn /norestart /i https://downloads.puppetlabs.com/windows/puppet5/puppet-agent-x64-latest.msi PUPPET_MASTER_SERVER=puppet
+Start-Process msiexec.exe -Wait -ArgumentList '/qn /norestart /i https://downloads.puppetlabs.com/windows/puppet5/puppet-agent-x64-latest.msi PUPPET_MASTER_SERVER=puppet'
 & "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs/windows
 & "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" apply octopus.pp --disable_warnings=deprecations
