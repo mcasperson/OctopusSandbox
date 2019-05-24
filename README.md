8d3ba7c3b198fd7ebbf7a762f1a718ac84c0742e
Matthew Casperson <matthewcasperson@gmail.com>
2019-05-24 10:34:11 +1000

    Fied installer

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 This repository contains scripts for populating test Windows instances with an Octopus Deploy CI/CD workflow.
 
-Install in Powershell with the command:
+Install in an administrative Powershell session with the command:
 
 `Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/mcasperson/OctopusSandbox/master/install.ps1'))`
 
--- install.ps1
+++ install.ps1
@@ -1,4 +1,8 @@
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/mcasperson/OctopusSandbox/master/octopus.pp -OutFile octopus.pp
 Start-Process msiexec.exe -Wait -ArgumentList '/qn /norestart /i https://downloads.puppetlabs.com/windows/puppet5/puppet-agent-x64-latest.msi PUPPET_MASTER_SERVER=puppet'
-& "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs/windows
-& "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" apply octopus.pp --disable_warnings=deprecations
+if (Test-Path "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat") {
+    & "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs/windows
+    & "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" apply octopus.pp --disable_warnings=deprecations
+} else {
+    Write-Error "Could not find the Puppet agent executable. Make sure the script was run from an administrative Powershell session."
+}
