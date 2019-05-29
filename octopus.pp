13c5486cc969cf1bccf67551d342b3775b697900
Matthew Casperson <matthewcasperson@gmail.com>
2019-05-29 14:15:16 +1000

    Added some default credentials

--- octopus.pp
+++ octopus.pp
@@ -109,6 +109,112 @@ file { 'C:/install/system.componentmodel.annotations.4.1.0':
   cleanup      => true,
 }
 
+# CONFIGURE OCTOPUS
+
+package { 'sql-server-express':
+  ensure   => installed,
+  provider => chocolatey
+}
+-> package { 'octopusdeploy':
+  ensure   => installed,
+  provider => chocolatey
+}
+-> file { 'C:/install_octopus.bat':
+  ensure  => 'file',
+  owner   => 'Administrators',
+  group   => 'Administrators',
+  mode    => '0644',
+  content => @(EOT)
+    "C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" create-instance --instance "OctopusServer" --config "C:\Octopus\OctopusServer.config"
+    "C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" database --instance "OctopusServer" --connectionString "Data Source=(local)\SQLEXPRESS;Initial Catalog=Octopus;Integrated Security=True" --create --grant "NT AUTHORITY\SYSTEM"
+    "C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" configure --instance "OctopusServer" --upgradeCheck "False" --upgradeCheckWithStatistics "False" --webForceSSL "False" --webListenPrefixes "http://localhost:80/" --commsListenPort "10943" --serverNodeName "DESKTOP-JVNRAAG" --usernamePasswordIsEnabled "True"
+    "C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" service --instance "OctopusServer" --stop
+    "C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" admin --instance "OctopusServer" --username "admin" --email "a@a.com" --password "Password01!"
+    "C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" service --instance "OctopusServer" --install --reconfigure --start --dependOn "MSSQL$SQLEXPRESS"
+    copy /y NUL C:\OctopusDeployInstalled.txt >NUL
+    | EOT
+}
+-> exec { 'Install Octopus':
+  command => 'C:\\Windows\\system32\\cmd.exe /c C:\\install_octopus.bat',
+  creates => 'C:/OctopusDeployInstalled.txt',
+}
+-> package { 'octopustools':
+  ensure   => installed,
+  provider => chocolatey
+}
+-> exec{'Create Octopus Shortcut':
+    provider => 'powershell',
+    command  => '$sh = New-Object -comObject WScript.Shell; $short = $sh.CreateShortcut($sh.SpecialFolders("Desktop") + "\\Octopus.lnk"); $short.TargetPath = "http://localhost"; $short.Save();'
+}
+-> file { 'C:/initialise_octopus.ps1':
+  ensure  => 'file',
+  owner   => 'Administrators',
+  group   => 'Administrators',
+  mode    => '0644',
+  content => @(EOT)
+    Add-Type -Path "C:/install/system.componentmodel.annotations.4.1.0/lib/netstandard1.4/System.ComponentModel.Annotations.dll"
+    Add-Type -Path "C:/install/newtonsoft.json.9.0.1/lib/netstandard1.0/Newtonsoft.Json.dll"
+    Add-Type -Path "C:/install/octopus.client.6.7.0/lib/netstandard2.0/Octopus.Client.dll"
+
+    #Creating a connection
+    $endpoint = new-object Octopus.Client.OctopusServerEndpoint "http://localhost"
+    $repository = new-object Octopus.Client.OctopusRepository $endpoint
+
+    #Creating login object
+    $LoginObj = New-Object Octopus.Client.Model.LoginCommand 
+    $LoginObj.Username = "admin"
+    $LoginObj.Password = "Password01!"
+
+    #Loging in to Octopus
+    $repository.Users.SignIn($LoginObj)
+
+    #Getting current user logged in
+    $UserObj = $repository.Users.GetCurrent()
+
+    #Creating API Key for user. This automatically gets saved to the database.
+    $ApiObj = $repository.Users.CreateApiKey($UserObj, "Puppet Install")
+
+    #Save the API key so we can use it later
+    Set-Content -Path c:\octopus_api_key.txt -Value $ApiObj.ApiKey
+
+    #Create the standard environments
+    & C:\ProgramData\chocolatey\bin\octo.exe create-environment --name=Dev --apiKey=$($ApiObj.ApiKey) --server=http://localhost --ignoreIfExists
+    & C:\ProgramData\chocolatey\bin\octo.exe create-environment --name=Test --apiKey=$($ApiObj.ApiKey) --server=http://localhost --ignoreIfExists
+    & C:\ProgramData\chocolatey\bin\octo.exe create-environment --name=Prod --apiKey=$($ApiObj.ApiKey) --server=http://localhost --ignoreIfExists
+
+    Set-Content -Path C:\octopus_api.txt -Value $($ApiObj.ApiKey)
+    | EOT
+}
+-> exec { 'Populate Environments':
+  command  => '& C:/initialise_octopus.ps1',
+  provider => powershell,
+}
+
+package { 'octopusdeploy.tentacle':
+  ensure   => installed,
+  provider => chocolatey
+}
+-> file { 'C:/install_tentacle.bat':
+  ensure  => 'file',
+  owner   => 'Administrators',
+  group   => 'Administrators',
+  mode    => '0644',
+  content => @(EOT)
+    "C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" create-instance --instance "Tentacle" --config "C:\Octopus\Tentacle.config"
+    "C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" new-certificate --instance "Tentacle" --if-blank
+    "C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" configure --instance "Tentacle" --reset-trust
+    "C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" configure --instance "Tentacle" --app "C:\Octopus\Applications" --port "10933" --noListen "True"
+    "C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" polling-proxy --instance "Tentacle" --proxyEnable "False" --proxyUsername "" --proxyPassword "" --proxyHost "" --proxyPort ""
+    "C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" register-with --instance "Tentacle" --server "http://localhost" --name "WindowsSandbox" --comms-style "TentacleActive" --server-comms-port "10943" --username "admin" --password "Password01!" --space "Default" --environment "Dev" --role "Windows" --policy "Default Machine Policy"
+    "C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" service --instance "Tentacle" --install --stop --start
+    copy /y NUL C:\OctopusTentacleInstalled.txt >NUL
+    | EOT
+}
+-> exec { 'Install Tentacle':
+  command => 'C:\\Windows\\system32\\cmd.exe /c C:\\install_tentacle.bat',
+  creates => 'C:/OctopusTentacleInstalled.txt',
+}
+
 # CONFIGURE JENKINS
 
 file { 'C:/program Files (x86)/Jenkins/init.groovy.d':
@@ -159,7 +265,7 @@ file { 'C:/program Files (x86)/Jenkins/init.groovy.d':
 
     // The list of plugins to install
     Set<String> plugins_to_install = [
-        "git", "github", "blueocean", "custom-tools-plugin", "simple-theme-plugin"
+        "git", "github", "blueocean", "custom-tools-plugin", "simple-theme-plugin", "plain-credentials"
     ]
 
     /*
@@ -237,6 +343,89 @@ file { 'C:/program Files (x86)/Jenkins/init.groovy.d':
 
     | EOT
 }
+-> file { 'C:/Program Files (x86)/Jenkins/init.groovy.d/d.secrets.groovy':
+  ensure    => 'file',
+  owner     => 'Administrators',
+  group     => 'Administrators',
+  mode      => '0644',
+  content   => @(EOT)
+    #!groovy
+    import jenkins.model.*
+    import com.cloudbees.plugins.credentials.*
+    import com.cloudbees.plugins.credentials.common.*
+    import com.cloudbees.plugins.credentials.domains.*
+    import com.cloudbees.plugins.credentials.impl.*
+    import com.cloudbees.jenkins.plugins.sshcredentials.impl.*
+    import org.jenkinsci.plugins.plaincredentials.*
+    import org.jenkinsci.plugins.plaincredentials.impl.*
+    import hudson.util.Secret
+    import hudson.plugins.sshslaves.*
+    import org.apache.commons.fileupload.* 
+    import org.apache.commons.fileupload.disk.*
+    import java.nio.file.Files
+
+    domain = Domain.global()
+    store = Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
+
+    store.addCredentials(
+      domain, 
+      new StringCredentialsImpl(
+        CredentialsScope.GLOBAL,
+        "APIKey",
+        "Octopus API Key",
+        Secret.fromString(new File("C:\octopus_api.txt").text)))
+
+    store.addCredentials(
+      domain, 
+      new StringCredentialsImpl(
+        CredentialsScope.GLOBAL,
+        "OctopusServer",
+        "Octopus Server URL",
+        Secret.fromString("http://localhost")))
+
+    /*
+    priveteKey = new BasicSSHUserPrivateKey(
+    CredentialsScope.GLOBAL,
+    "jenkins-slave-key",
+    "root",
+    new BasicSSHUserPrivateKey.UsersPrivateKeySource(),
+    "",
+    ""
+    )
+
+    store.addCredentials(domain, priveteKey)
+    */
+
+    /*
+    usernameAndPassword = new UsernamePasswordCredentialsImpl(
+      CredentialsScope.GLOBAL,
+      "jenkins-slave-password", "Jenkis Slave with Password Configuration",
+      "root",
+      "jenkins"
+    )
+
+    store.addCredentials(domain, usernameAndPassword)
+    */    
+
+    /*
+    factory = new DiskFileItemFactory()
+    dfi = factory.createItem("", "application/octet-stream", false, "filename")
+    out = dfi.getOutputStream()
+    file = new File("/path/to/some/file")
+    Files.copy(file.toPath(), out)
+    FileCredentailsImpl can take a file from a do
+    secretFile = new FileCredentialsImpl(
+    CredentialsScope.GLOBAL,
+    "secret-file",
+    "Secret File Description"
+    dfi, // Don't use FileItem
+    "",
+    "")
+    
+    store.addCredentials(domain, secretFile)
+    */
+    | EOT
+}
 -> file_line { 'installStateName':
   path      => 'C:/Program Files (x86)/Jenkins/config.xml',
   line      => '  <installStateName>RUNNING</installStateName>',
@@ -251,106 +440,4 @@ file { 'C:/program Files (x86)/Jenkins/init.groovy.d':
     command  => '$sh = New-Object -comObject WScript.Shell; $short = $sh.CreateShortcut($sh.SpecialFolders("Desktop") + "\\Jenkins.lnk"); $short.TargetPath = "http://localhost:8080"; $short.Save();'
 }
 
-# CONFIGURE OCTOPUS
 
-package { 'sql-server-express':
-  ensure   => installed,
-  provider => chocolatey
-}
--> package { 'octopusdeploy':
-  ensure   => installed,
-  provider => chocolatey
-}
--> file { 'C:/install_octopus.bat':
-  ensure  => 'file',
-  owner   => 'Administrators',
-  group   => 'Administrators',
-  mode    => '0644',
-  content => @(EOT)
-    "C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" create-instance --instance "OctopusServer" --config "C:\Octopus\OctopusServer.config"
-    "C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" database --instance "OctopusServer" --connectionString "Data Source=(local)\SQLEXPRESS;Initial Catalog=Octopus;Integrated Security=True" --create --grant "NT AUTHORITY\SYSTEM"
-    "C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" configure --instance "OctopusServer" --upgradeCheck "False" --upgradeCheckWithStatistics "False" --webForceSSL "False" --webListenPrefixes "http://localhost:80/" --commsListenPort "10943" --serverNodeName "DESKTOP-JVNRAAG" --usernamePasswordIsEnabled "True"
-    "C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" service --instance "OctopusServer" --stop
-    "C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" admin --instance "OctopusServer" --username "admin" --email "a@a.com" --password "Password01!"
-    "C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" service --instance "OctopusServer" --install --reconfigure --start --dependOn "MSSQL$SQLEXPRESS"
-    copy /y NUL C:\OctopusDeployInstalled.txt >NUL
-    | EOT
-}
--> exec { 'Install Octopus':
-  command => 'C:\\Windows\\system32\\cmd.exe /c C:\\install_octopus.bat',
-  creates => 'C:/OctopusDeployInstalled.txt',
-}
--> package { 'octopustools':
-  ensure   => installed,
-  provider => chocolatey
-}
--> exec{'Create Octopus Shortcut':
-    provider => 'powershell',
-    command  => '$sh = New-Object -comObject WScript.Shell; $short = $sh.CreateShortcut($sh.SpecialFolders("Desktop") + "\\Octopus.lnk"); $short.TargetPath = "http://localhost"; $short.Save();'
-}
--> file { 'C:/initialise_octopus.ps1':
-  ensure  => 'file',
-  owner   => 'Administrators',
-  group   => 'Administrators',
-  mode    => '0644',
-  content => @(EOT)
-    Add-Type -Path "C:/install/system.componentmodel.annotations.4.1.0/lib/netstandard1.4/System.ComponentModel.Annotations.dll"
-    Add-Type -Path "C:/install/newtonsoft.json.9.0.1/lib/netstandard1.0/Newtonsoft.Json.dll"
-    Add-Type -Path "C:/install/octopus.client.6.7.0/lib/netstandard2.0/Octopus.Client.dll"
-
-    #Creating a connection
-    $endpoint = new-object Octopus.Client.OctopusServerEndpoint "http://localhost"
-    $repository = new-object Octopus.Client.OctopusRepository $endpoint
-
-    #Creating login object
-    $LoginObj = New-Object Octopus.Client.Model.LoginCommand 
-    $LoginObj.Username = "admin"
-    $LoginObj.Password = "Password01!"
-
-    #Loging in to Octopus
-    $repository.Users.SignIn($LoginObj)
-
-    #Getting current user logged in
-    $UserObj = $repository.Users.GetCurrent()
-
-    #Creating API Key for user. This automatically gets saved to the database.
-    $ApiObj = $repository.Users.CreateApiKey($UserObj, "Puppet Install")
-
-    #Save the API key so we can use it later
-    Set-Content -Path c:\octopus_api_key.txt -Value $ApiObj.ApiKey
-
-    #Create the standard environments
-    & C:\ProgramData\chocolatey\bin\octo.exe create-environment --name=Dev --apiKey=$($ApiObj.ApiKey) --server=http://localhost --ignoreIfExists
-    & C:\ProgramData\chocolatey\bin\octo.exe create-environment --name=Test --apiKey=$($ApiObj.ApiKey) --server=http://localhost --ignoreIfExists
-    & C:\ProgramData\chocolatey\bin\octo.exe create-environment --name=Prod --apiKey=$($ApiObj.ApiKey) --server=http://localhost --ignoreIfExists
-    | EOT
-}
--> exec { 'Populate Environments':
-  command  => '& C:/initialise_octopus.ps1',
-  provider => powershell,
-}
-
-package { 'octopusdeploy.tentacle':
-  ensure   => installed,
-  provider => chocolatey
-}
--> file { 'C:/install_tentacle.bat':
-  ensure  => 'file',
-  owner   => 'Administrators',
-  group   => 'Administrators',
-  mode    => '0644',
-  content => @(EOT)
-    "C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" create-instance --instance "Tentacle" --config "C:\Octopus\Tentacle.config"
-    "C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" new-certificate --instance "Tentacle" --if-blank
-    "C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" configure --instance "Tentacle" --reset-trust
-    "C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" configure --instance "Tentacle" --app "C:\Octopus\Applications" --port "10933" --noListen "True"
-    "C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" polling-proxy --instance "Tentacle" --proxyEnable "False" --proxyUsername "" --proxyPassword "" --proxyHost "" --proxyPort ""
-    "C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" register-with --instance "Tentacle" --server "http://localhost" --name "WindowsSandbox" --comms-style "TentacleActive" --server-comms-port "10943" --username "admin" --password "Password01!" --space "Default" --environment "Dev" --role "Windows" --policy "Default Machine Policy"
-    "C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" service --instance "Tentacle" --install --stop --start
-    copy /y NUL C:\OctopusTentacleInstalled.txt >NUL
-    | EOT
-}
--> exec { 'Install Tentacle':
-  command => 'C:\\Windows\\system32\\cmd.exe /c C:\\install_tentacle.bat',
-  creates => 'C:/OctopusTentacleInstalled.txt',
-}
